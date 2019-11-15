dee1faa84050304867d2ad3e98f9e44b9edacd70
zgs <284039930@qq.com>
2019-11-15 22:45:20 +0800

    饿了么外卖vue项目的index.js,在这配置路由

--- VUE/VUE-ELeMe-APP/vue-ele-app/src/router/index.js
+++ VUE/VUE-ELeMe-APP/vue-ele-app/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Goods from '@/components/goods/goods'
+import ratings from '@/components/ratings/ratings'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/goods',
       name: 'Goods',
       component: Goods
+    },
+    {
+      path: '/ratings',
+      name: '/ratings',
+      component: ratings
     }
   ]
 })
