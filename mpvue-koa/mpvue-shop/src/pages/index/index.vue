cb2057edca45c165cd2530329ecd325b38770422
zgs <284039930@qq.com>
2020-02-15 21:17:05 +0800

    网易严选小程序主页面

--- mpvue-koa/mpvue-shop/src/pages/index/index.vue
+++ mpvue-koa/mpvue-shop/src/pages/index/index.vue
@@ -18,6 +18,116 @@
         </block>
       </swiper>
     </div>
+    <!-- 类别选择 -->
+    <div class="channel">
+      <div v-for="(item, index) in channel" :key="index" @click="categroyList(item.id)">
+        <img :src="item.icon_url" alt="">
+        <p>{{item.name}}</p>
+      </div>
+    </div>
+    <!-- 品牌制造商直供 -->
+    <div class="brand">
+      <div class="head" @click="tobrandList()">
+        品牌制造商直供
+      </div>
+      <div class="content">
+        <div v-for="(item, index) in brandList" :key="index" @click="branddetail(item.id)">
+          <div>
+            <p>{{item.name}}</p>
+            <p class="price">{{item.floor_price}}元起</p>
+          </div>
+          <img :src="item.new_pic_url" alt="">
+        </div>
+      </div>
+    </div>
+    <!-- 新品首发 -->
+    <div class="newgoods">
+      <div class="newgoods-top" @click="goodsList('new')">
+        <div class="top">
+          <p>新品首发</p>
+          <p>查看全部</p>
+        </div>
+      </div>
+      <div class="list">
+        <ul>
+          <scroll-view class="scroll-view" :scroll-x="true">
+            <!-- 左右滚动 -->
+            <li v-for="(item, index) in newGoods" :key="index">
+              <img :src="item.list_pic_url" alt="">
+              <p>{{item.name}}</p>
+              <p>{{item.goods_brief}}</p>
+              <p>¥{{item.retail_price}}</p>
+            </li>
+          </scroll-view>
+        </ul>
+      </div>
+    </div>
+    <!-- 人气推荐 -->
+    <div class="newgoods hotgoods">
+      <div class="newgoods-top" @click="goodsList('hot')">
+        <div class="top">
+          <p>
+            人气推荐
+            <span></span>
+            好物精选
+          </p>
+          <p>查看全部</p>
+        </div>
+      </div>
+      <div class="list">
+        <ul>
+          <scroll-view class="scroll-view" :scroll-x="true">
+            <li v-for="(item ,index) in hotGoods" :key="index">
+              <img :src="item.list_pic_url" alt="">
+              <p>{{item.name}}</p>
+              <p>{{item.goods_brief}}</p>
+              <p>¥{{item.retail_price}}</p>
+            </li>
+          </scroll-view>
+        </ul>
+      </div>
+    </div>
+    <!-- 专题精选 -->
+    <div class="topicList">
+      <div class="topicList-top">
+        专题精选
+        <span class="icon"></span>
+      </div>
+      <div class="list">
+        <ul>
+          <scroll-view class="scroll-view" :scroll-x="true">
+            <li v-for="(item ,index) in topicList" :key="index" @click="topicdetail(item.id)">
+              <img :src="item.item_pic_url" alt="">
+              <div class="btom">
+                <div>
+                  <p>{{item.title}}</p>
+                  <p>{{item.subtitle}}</p>
+                </div>
+                <div>{{item.price_info}}元起</div>
+              </div>
+            </li>
+          </scroll-view>
+        </ul>
+      </div>
+    </div>
+    <div class="newcategory">
+      <div class="list" v-for="(item ,index) in newCategoryList" :key="index">
+        <div class="head">{{item.name}}好物</div>
+        <div class="sublist">
+          <div v-for="(subitem, subindex) in item.goodsList" :key="subindex">
+            <img :src="subitem.list_pic_url" alt="">
+            <p>{{subitem.name}}</p>
+            <p>{{subitem.retail_price}}</p>
+          </div>
+          <div>
+            <div class="last">
+              <p>{{item.name}}好物</p>
+              <span class="icon"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,11 +138,19 @@ import { get } from '../../utils/index'
 export default {
   data () {
     return {
-      banner: []
+      banner: [],
+      channel: [],
+      brandList: [],
+      newGoods: [],
+      hotGoods: []
     }
   },
   computed: {
     ...mapState(['cityName'])
+  },
+  mounted () {
+    this.getData() // 获取轮播图数据
+    this.getCityName() // 定位
   },
   methods: {
     // 定位地点的页面(mappage)
@@ -85,9 +203,44 @@ export default {
 
     // 主页的逻辑(index)
     async getData() {
-      // 获取轮播的图片(banner)
+      // 从后端获取首页需要的数据
       const data = await get('/index/index')  // http://localhost:5757/lm/index/index
-      console.log(data)
+      console.log(data)  // 打印出了从后端获取的数据
+      this.banner = data.banner
+      this.channel = data.channel
+      this.brandList = data.brandList
+      this.newGoods = data.newGoods
+      this.hotGoods = data.hotGoods
+    },
+    categroyList(id) {
+      // 种类(居家 餐厨 等等) 页面跳转
+      wx.navigateTo({
+        url: '/pages/categroylist/main?id=' + id 
+      });
+    },
+    branddetail (id) {
+      // 品牌制造商详情页面跳转
+      wx.navigateTo({
+        url: '/pages/branddetail/main?id=' + id
+      });
+    },
+    tobrandList () {
+      // 跳转到查看所有品牌制造商的页面
+      wx.navigateTo({ 
+        url: '/pages/brandlist/main'
+      })
+    },
+    goodsList (info) {
+      // 根据传参不同跳转到新品页面或者人气推荐页面(同一个页面内容不同)
+      if (info == 'hot') {
+        wx.navigateTo({ 
+        url: '/pages/newgoods/main?isHot=' + 1
+      })
+      } else {
+        wx.navigateTo({ 
+        url: '/pages/newgoods/main?isNew=' + 1
+      })
+      }
     }
   }
 }
