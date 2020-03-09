716b64b107131bf797e66f6471761051f12d42d9
zgs <284039930@qq.com>
2020-03-09 22:37:17 +0800

    控制不同功能需要用到的代码

--- mpvue-koa/mpvue-shop-node/controllers/search/index.js
+++ mpvue-koa/mpvue-shop-node/controllers/search/index.js
@@ -21,9 +21,35 @@ async function indexAction(ctx) {
   }
 }
 
+// 搜索时匹配搜索相关内容
+async function helperAction(ctx) {
+  const keyword = ctx.query.keyword
+  // console.log(keyword)
+  var order = ctx.query.order
+  if (!order) {
+    order = ''
+    orderBy = 'id'
+  } else {
+    orderBy = 'retail_price'
+  }
+  // keywords是 十条包含keyword的搜索匹配数据
+  const keywords = await mysql('nideshop_goods').orderBy(orderBy, order)
+  .column('id', 'name', 'list_pic_url', 'retail_price')
+  .where('name', 'like', '%' + keyword + '%').limit(10).select()
+  if (keywords) {
+    ctx.body = {
+      keywords
+    }
+  } else {
+    ctx.body = {
+      keywords: []
+    }
+  }
+}
+
 // 添加搜索历史
 async function addHistoryAction(ctx) {
-  const {openId, keyword} = ctx.request.body
+  const {openId, keyword} = ctx.request.body // 需要安装 npm i koa-bodyparser
   const oldData = await mysql('nideshop_search_history').where({
     // 先检索一下 搜索历史的表 是否已经有当前搜索的内容
     'user_id': openId,
@@ -53,7 +79,28 @@ async function addHistoryAction(ctx) {
   }
 }
 
+// 清除搜索历史记录
+async function clearHistoryAction(ctx) {
+  const openId = ctx.request.body.openId
+  // console.log(openId)
+  const data = await mysql('nideshop_search_history').where({
+    'user_id': openId
+  }).del()
+  console.log(data) // 删除的所有数据的长度
+  if (data) {
+    ctx.body = {
+      'data': '清除成功'
+    }
+  } else {
+    ctx.body = {
+      'data': null
+    }
+  }
+}
+
 module.exports = {
   addHistoryAction,
-  indexAction
+  indexAction,
+  clearHistoryAction,
+  helperAction
 }
