d3d20d733116e03a9093d4ad72a62cd20548374c
zgs <284039930@qq.com>
2020-03-10 17:14:53 +0800

    管着与页面相关的组件

--- react/blog/src/Router/web.js
+++ react/blog/src/Router/web.js
@@ -1,15 +1,45 @@
 import loadable from '../utils/loadable'
 
-const List = loadable(() => import('../pages/web/list/list'))
-// const About = loadable(() => import('../pages/web/about'))
+import {
+  HomeOutlined, 
+  EditOutlined,
+  StarOutlined,
+  TeamOutlined
+} from '@ant-design/icons'
+
+const List = loadable(() => import('../pages/web/list/list'))  // 首页
+const Archive = loadable(() => import('../pages/web/archive'))   // 归档
+const Collect = loadable(() => import('../pages/web/collect'))   // 收藏
+const About = loadable(() => import('../pages/web/about'))   // 关于
 
 const webRoutes = [
   {
     menu: true,
-    icon: 'HomeOutlined',
+    icon: HomeOutlined,
     title: '首页',
     path: '/web/index',
     component: List
+  },
+  {
+    menu: true,
+    icon: EditOutlined,
+    title: '归档',
+    path: '/web/archive',
+    component: Archive
+  },
+  {
+    menu: true,
+    icon: StarOutlined,
+    title: '收藏',
+    path: '/web/collect',
+    component: Collect
+  },
+  {
+    menu: true,
+    icon: TeamOutlined,
+    title: '关于',
+    path: '/web/about',
+    component: About
   }
 ]
 
