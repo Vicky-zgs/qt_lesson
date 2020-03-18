3e6db0b0ee81163fb7028dc60264cddb76e0c3e9
zgs <284039930@qq.com>
2020-03-18 21:18:31 +0800

    简书的头部组件

--- react/jianshu/src/common/header/index.js
+++ react/jianshu/src/common/header/index.js
@@ -1,7 +1,9 @@
 // 所有页面都有的头部
 
-import React, { Component } from 'react'
+import React from 'react'
 import { CSSTransition } from 'react-transition-group'
+import { connect } from 'react-redux'
+import { actionCreators } from './store'
 import {
   HeaderWrapper,
   Logo,
@@ -13,69 +15,69 @@ import {
   SearchWrapper
 } from './style.js'
 
-class Header extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      focused: false
-    }
-    this.handleInputFocus = this.handleInputFocus.bind(this)
-    this.handleInputBlur = this.handleInputBlur.bind(this)
-  }
+const Header = (props) => {
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className="left active">首页</NavItem>
+        <NavItem className="left">下载App</NavItem>
+        <NavItem className="right">登录</NavItem>
+        <NavItem className="right">
+          <span className="iconfont">&#xe636;</span>
+        </NavItem>
+        <SearchWrapper>
+          <CSSTransition 
+            // 控制动画
+            timeout={200} 
+            in={props.focused}
+            className="slide"
+          >
+            {/* slide-enter slide-enter-active   slide-exit slide-exit-active */}
+            <NavSearch
+              className={props.focused ? 'focused' : ''}
+              // input框聚焦时就会触发onFocus方法
+              onFocus={props.handleInputFocus}
+              onBlur={props.handleInputBlur}  // input框失焦
+            ></NavSearch>
+          </CSSTransition>
+          <span className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe623;</span>
+        </SearchWrapper>
+      </Nav>
+      <Addition>
+        <Button className="writting">
+          <span className="iconfont">&#xe7c2;</span>
+          写文章
+        </Button>
+        <Button className="reg">注册</Button>
+      </Addition>
+    </HeaderWrapper>
+  )
+}
 
-  handleInputFocus() {
-    // 聚焦input框后给input框添加focused的类名
-    this.setState({
-      focused: true
-    })
-  }
-
-  handleInputBlur() {
-    // input框失焦
-    this.setState({
-      focused: false
-    })
-  }
-
-  render() {
-    return (
-      <HeaderWrapper>
-        <Logo />
-        <Nav>
-          <NavItem className="left active">首页</NavItem>
-          <NavItem className="left">下载App</NavItem>
-          <NavItem className="right">登录</NavItem>
-          <NavItem className="right">
-            <span className="iconfont">&#xe636;</span>
-          </NavItem>
-          <SearchWrapper>
-            <CSSTransition 
-              // 控制动画
-              timeout={200} 
-              in={this.state.focused}
-              className="slide"
-            >
-              {/* slide-enter slide-enter-active   slide-exit slide-exit-active */}
-              <NavSearch
-                className={this.state.focused ? 'focused' : ''}
-                // input框聚焦时就会触发onFocus方法
-                onFocus={this.handleInputFocus}
-                onBlur={this.handleInputBlur}  // input框失焦
-              ></NavSearch>
-            </CSSTransition>
-            <span className={this.state.focused ? 'focused iconfont' : 'iconfont'}>&#xe623;</span>
-          </SearchWrapper>
-        </Nav>
-        <Addition>
-          <Button className="writting">
-            <span className="iconfont">&#xe7c2;</span>
-            写文章
-          </Button>
-          <Button className="reg">注册</Button>
-        </Addition>
-      </HeaderWrapper>
-    )
+const mapStateToProps = (state) => {
+  return {
+    focused: state.header.get('focused')
   }
 }
 
-export default Header
+// store.dispatch ===> props
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocus() {
+      // input搜索框聚焦时加上focused类名 
+      // actionCreators.js 中会生成action
+      dispatch(actionCreators.searchFocus())
+    },
+    handleInputBlur() {
+      // input搜索框失焦时去掉focused类名
+      // const action = {
+      //   type: 'search_blur'
+      // }
+      // dispatch(action)
+      dispatch(actionCreators.searchBlur())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
---
+++ react/jianshu/src/common/header/store/reducer.js
@@ -0,0 +1,20 @@
+import * as types from './actionTypes'
+import { fromJS } from 'immutable'
+// immutable.js
+// facebook
+// immutable对象   不可改变的对象
+
+const defaultState = fromJS({
+  focused: false
+})
+
+export default (state = defaultState, action) => {
+  if (action.type === types.SEARCH_FOCUS) {
+    // immutable对象的set方法, 会结合之前immutable对象的值和设置的值返回一个全新的对象
+    return state.set('focused', true)
+  }
+  if (action.type === types.SEARCH_BLUR) {
+    return state.set('focused', false)
+  }
+  return state
+}
