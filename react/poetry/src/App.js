b3fe520c5797caafcd80a9d026469285ffcbd8dd
ZhouGuangSi <284039930@qq.com>
2020-04-28 22:02:52 +0800

    古诗词

--- react/poetry/src/App.js
+++ react/poetry/src/App.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store/index.js'
 import Login from './pages/admin/login'
 import 'antd/dist/antd.css'
 import routes from './Router/web'
 import WebLayout from './pages/web/layout/index'
-import HeaderCustom from './pages/web/header/index'
+import PersonalInfo from './pages/web/personalInfo/index.js'
+import CollectionManage from './pages/web/manage/collection/index.js'
 
 class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <HeaderCustom />
-          <div>
-            <Route exact path="/" component={WebLayout}></Route>
-            <Route path="/login" component={Login} />
-            {
-              routes.map((route, i) => (
-                <Route key={i} path={route.path} component={route.component} />
-              ))
-            }
-          </div>
-        </Router>
-      </Provider>
+      <Router>
+        <div>
+          <Route exact path="/" component={WebLayout}></Route>
+          <Route path="/login" component={Login} />
+          {
+            routes.map((route, i) => (
+              <Route key={i} path={route.path} component={route.component} />
+            ))
+          }
+          <Route path="/personalInfo" component={PersonalInfo}></Route>
+          <Route path="/collectionManage" component={CollectionManage}></Route>
+        </div>
+      </Router>
     )
   }
 }
