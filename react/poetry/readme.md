0f876cf5ebc4bf30107046ec48dcf09136d6f7d3
ZhouGuangSi <284039930@qq.com>
2020-06-26 20:54:51 +0800

    古诗词系统

--- react/poetry/readme.md
+++ react/poetry/readme.md
@@ -18,6 +18,27 @@
   - web/layout 主页面
   - web/list   主页面中的展示列表组件
 
+代码层次：
+App.js 路由配置
+/src/pages/admin 登录组件  和  管理员的管理组件
+/src/pages/web/header    头部组件
+              /ancient   和 author 、 poetry 、 rhesis  导航栏对应组件
+              /component 可复用的列表组件和放标签的组件
+              /manage    用户管理自己的信息的组件
 
-5.1 记录
-- 接下来要写管理员的管理页面，写个table组件，放到管理组件的content里，然后每次点击不同的submenu就改变当前页面的state中的url，传不同的url接口给table，就可以传回来不同的数据
+- 点击不同诗词 跳转到不同内容的诗词详情页面逻辑：
+  在诗词页面中 传递一个id值给子组件list组件，然后list组件通过this.props.match.params拿到id 再拿到不同的数据的值
+  - 点击不同朝代 返回不同内容的页面 同理
+
+- 刷新当前页面 window.location.reload(true)
+
+- 添加背景图片：
+  - import img from '../../../utils/img.png'
+  - const loginStyle = {
+        backgroundImage: `url(${img})`,
+        backgroundRepeat: 'repeat-y',
+        backgroundAttachment: 'fixed',
+        backgroundSize: '100% 100%',
+        opacity: '1'
+      }
+  - <div className="login" style={loginStyle}>
--- react/poetry/src/App.js
+++ react/poetry/src/App.js
@@ -6,11 +6,35 @@ import store from './store/index.js'
 import Login from './pages/admin/login'
 import 'antd/dist/antd.css'
 import routes from './Router/web'
-import WebLayout from './pages/web/layout/index'
+
+import ForgetPwd from './pages/admin/forgetPwd/index.js'
+import SetNewPwd from './pages/admin/modifyPwd/index.js'
+import WebLayout from './pages/web/recommend/index'
 import PersonalInfo from './pages/web/personalInfo/index.js'
+
 import CollectionManage from './pages/web/manage/collection/index.js'
 import CommentManage from './pages/web/manage/comment/index.js'
-import Manager from './pages/admin/manage/index.js'
+import LookNotice from './pages/web/manage/notice/index.js'
+import PoetInfo from './pages/web/component/poetInfo/index.js'
+import PoetryInfo from './pages/web/component/poetryInfo/index.js'
+
+import ManagerColl from './pages/admin/manage/collection/index.js'
+import ManagerMyInfo from './pages/admin/manage/myInfo/index.js'
+import ManageNotice from './pages/admin/manage/notice/index.js'
+import PubNotice from './pages/admin/manage/notice/pubNotice/index.js'
+import ManageComment from './pages/admin/manage/comment/index.js'
+import ManageUser from './pages/admin/manage/userInfo/index.js'
+import ManagePoetry from './pages/admin/manage/poetry_all/poetry/index.js'
+import ManagePoetryDynasty from './pages/admin/manage/poetry_all/poetryDynasty/index.js'
+import ManagePoetryType from './pages/admin/manage/poetry_all/poetryType/index.js'
+import ManagePoet from './pages/admin/manage/poetry_all/poet/index.js'
+import ManageUpload from './pages/admin/manage/upload/index.js'
+
+import DynastyPoetry from './pages/web/poetry/dynasty/index.js'
+// import DynastyPoetry from './pages/web/poetry/dynasty/index.js'
+import TypePoetry from './pages/web/poetry/type/index.js'
+
+import Search from './pages/web/poetry/search_after/index.js'
 
 class App extends React.Component {
   render() {
@@ -25,10 +49,39 @@ class App extends React.Component {
               <Route key={i} path={route.path} component={route.component} />
             ))
           }
+          <Route path="/forgetPwd" component={ForgetPwd} />
+          <Route path="/setNewPassword" component={SetNewPwd} />
+
+          {/* 用户的管理 */}
           <Route path="/personalInfo" component={PersonalInfo}></Route>
           <Route path="/collectionManage" component={CollectionManage}></Route>
-          <Route path="/admin/manager" component={Manager}></Route>
           <Route path="/commentManage" component={CommentManage}></Route>
+
+          {/* 诗人、诗词详情页 */}
+          <Route path="/poetInfo/:id" component={PoetInfo}></Route>
+          <Route path="/poetryInfo/:id/:poetryname" component={PoetryInfo}></Route>
+
+          {/* 管理员的管理 */}
+          <Route path="/admin/collection" component={ManagerColl}></Route>
+          <Route path="/admin/myInfo" component={ManagerMyInfo}></Route>
+          <Route path="/admin/notice" component={ManageNotice}></Route>
+          <Route path="/admin/pubNotice" component={PubNotice}></Route>
+          <Route path="/admin/manageComment" component={ManageComment}></Route>
+          <Route path="/admin/manageUser" component={ManageUser}></Route>
+          {/* 管理员管理 -> 古诗词管理 */}
+          <Route path="/admin/managePoetry" component={ManagePoetry}></Route>
+          <Route path="/admin/managePoetryDynasty" component={ManagePoetryDynasty}></Route>
+          <Route path="/admin/managePoetryType" component={ManagePoetryType}></Route>
+          <Route path="/admin/managePoet" component={ManagePoet}></Route>
+          <Route path="/admin/manageUpload" component={ManageUpload}></Route>
+
+          {/* 点击不同朝代后跳转的诗词页面 */}
+          <Route path="/poetry/dynasty/:id" component={DynastyPoetry}></Route>
+          <Route path="/poetry/type/:id" component={TypePoetry}></Route>
+
+          {/* 点击搜索后跳转的页面 */}
+          <Route path="/web/search/:id" component={Search}></Route>
+
         </div>
       </Router>
       </Provider>
